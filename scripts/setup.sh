b052587b313932e6dce925f791e99bc7c4a94eba
Scott Walter <sjwalter@gmail.com>
2024-02-07 16:54:48 +0000

    fix codespace init

---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+npm i -g obsidian-plugin-cli auto @auto-it/git-tag @auto-it/all-contributors @auto-it/conventional-commits auto-plugin-obsidian
+
+source /workspaces/obsidian-snippet-downloader/.env
